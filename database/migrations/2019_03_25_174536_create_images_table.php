5790ca469bcea524ba910aa91f1b34cc72fbce72
Steve Harmeyer <harmeyersteve@gmail.com>
2019-03-25 10:48:35 -0700

    updates

---
+++ database/migrations/2019_03_25_174536_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('model');
+            $table->string('image');
+            $table->integer('weight')->default(0)->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
---
+++ src/Models/Image.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Submtd\LaravelModelImages\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Images extends Model
+{
+}
