b026c08102eecb36723d26776b6e0b083e88dd57
Steve Harmeyer <harmeyersteve@gmail.com>
2019-03-25 10:49:43 -0700

    updates

--- src/Providers/LaravelModelImagesServiceProvider.php
+++ src/Providers/LaravelModelImagesServiceProvider.php
@@ -14,5 +14,8 @@ class LaravelModelImagesServiceProvider extends ServiceProvider
         // config files
         $this->mergeConfigFrom(__DIR__ . '/../../config/laravel-model-images.php', 'laravel-model-images');
         $this->publishes([__DIR__ . '/../../config' => config_path()], 'config');
+        // set up migrations
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        $this->publishes([__DIR__ . '/../../database' => database_path()], 'migrations');
     }
 }
